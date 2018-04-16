87e819b2b0183d4c09f19a7448760b36abb5ea1a
Felipe Sere <felipesere@gmail.com>
2018-04-16 17:41:47 +0100

    Simplify tests and filter

--- src/reductionist/core.clj
+++ src/reductionist/core.clj
@@ -4,16 +4,14 @@
 (defn fs-reduce [function collection accumulator]
     (if (empty? collection)
       accumulator
-      (let [one  (first collection)
-            others (rest collection)
+      (let [[one & others] collection
             new-acc (function one accumulator)]
         (recur function others new-acc))))
-
 
 (defn fs-any? [function collection]
   (fs-reduce #(or (function %1) %2) collection false))
 
 
 (defn fs-filter [function collection]
-  (reverse 
-    (fs-reduce #(if (function %1) (cons %1 %2) %2) collection [])))
+  (fs-reduce #(if (function %1) (concat %2 [%1]) %2) collection []))
+
--- test/reductionist/core_test.clj
+++ test/reductionist/core_test.clj
@@ -2,26 +2,15 @@
   (:require [clojure.test :refer :all]
             [reductionist.core :refer :all :as core]))
 
-(deftest a-test
-  (testing "empty reducer"
-    (is (= 0 (core/fs-reduce #(+ %1 %2) [] 0))))
+(deftest reducer-tests
+    (is (= 0 (core/fs-reduce #(+ %1 %2) [] 0)))
+    (is (= 10 (core/fs-reduce  #(+ %1 %2) [1 2 3 4] 0))))
 
-  (testing "a reducer"
-    (is (= 10 (core/fs-reduce  #(+ %1 %2) [1 2 3 4] 0)))))
+(deftest any-tests
+    (is (= false (core/fs-any?  #(> %1 1) [])))
+    (is (= true (core/fs-any?  #(> %1 1) [4])))
+    (is (= false (core/fs-any?  #(> %1 10) [4 5 8]))))
 
-(deftest any
-  (testing "empty collection"
-    (is (= false (core/fs-any?  #(> %1 1) []))))
-
-  (testing "finds something"
-    (is (= true (core/fs-any?  #(> %1 1) [4]))))
-
-  (testing "finds nothing"
-    (is (= false (core/fs-any?  #(> %1 10) [4 5 8])))))
-
-(deftest filter
-  (testing "empty collection"
-    (is (= [] (core/fs-filter #(> %1 1) []))))
-
-  (testing "filter stuff out"
-    (is (= [2 7] (core/fs-filter #(> %1 1) [2 1 7])))))
+(deftest filter-test
+    (is (= [] (core/fs-filter #(> %1 1) [])))
+    (is (= [2 7] (core/fs-filter #(> %1 1) [2 1 7]))))
